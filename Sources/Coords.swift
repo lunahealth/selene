5398636ebc225a49c80ff2c6ee1759d3dd6a9b21
vaux <zero.griffin@gmail.com>
2021-12-12 14:50:23 +0100

    Get sun coords

--- Sources/Coords.swift
+++ Sources/Coords.swift
@@ -1,4 +1,18 @@
+import Foundation
+
 public struct Coords {
-    public let latitude: Double
-    public let longitude: Double
+    public let latitude: Double // declination
+    public let longitude: Double // right ascencion
+    
+    var equatorial: Self {
+        .init(latitude: declination, longitude: rightAscension)
+    }
+    
+    private var declination: Double {
+        asin(sin(latitude) * cos(Earth.Obliquity) + cos(latitude) * sin(Earth.Obliquity) * sin(longitude))
+    }
+    
+    private var rightAscension: Double {
+        atan2(sin(longitude) * cos(Earth.Obliquity) - tan(latitude) * sin(Earth.Obliquity), cos(longitude))
+    }
 }
---
+++ Sources/Earth.swift
@@ -0,0 +1,9 @@
+struct Earth {
+    static let Mean0 = 357.5291
+    static let Mean1 = 0.98560028
+    static let Center1 = 1.9148
+    static let Center2 = 0.02
+    static let Center3 = 0.0003
+    static let Perihelion = 102.9373.toRadians
+    static let Obliquity = 23.4393.toRadians
+}
