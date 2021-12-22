e543754c38eab6e39c19b9e4324b1a70e0fe2497
vaux <zero.griffin@gmail.com>
2021-12-22 15:02:42 +0100

    Update point

--- Sources/Wheel.swift
+++ Sources/Wheel.swift
@@ -4,11 +4,13 @@ private let cycle = 29.53
 private let cycleRadians = .pi2 / cycle
 
 public struct Wheel {
-    public let progress: Double
+    public let point: CGPoint
+    let progress: Double
     private let date: Date
     
-    public init(date: Date, moon: Moon) {
+    public init(date: Date, moon: Moon, correction: Double) {
         progress = .pi + moon.inclination * (moon.apparentAngle < 0 ? -1 : 1)
+        point = .init(x: cos(progress), y: sin(progress))
         self.date = date
     }
     
--- Tests/WheelTests.swift
+++ Tests/WheelTests.swift
@@ -6,33 +6,45 @@ final class WheelTests: XCTestCase {
         var moon = Moon(inclination: .pi, apparentAngle: 1)
         
         XCTAssertEqual(.new, Moon.Phase(inclination: moon.inclination, angle: moon.apparentAngle))
-        XCTAssertEqual(.pi2, Wheel(date: .now, moon: moon).progress)
+        XCTAssertEqual(.pi2, Wheel(date: .now, moon: moon, correction: 0).progress)
         
         moon = .init(inclination: 0, apparentAngle: 0)
         
         XCTAssertEqual(.full, Moon.Phase(inclination: moon.inclination, angle: moon.apparentAngle))
-        XCTAssertEqual(.pi, Wheel(date: .now, moon: moon).progress)
+        XCTAssertEqual(.pi, Wheel(date: .now, moon: moon, correction: 0).progress)
         
         moon = .init(inclination: .pi_2, apparentAngle: -1)
         
         XCTAssertEqual(.firstQuarter, Moon.Phase(inclination: moon.inclination, angle: moon.apparentAngle))
-        XCTAssertEqual(.pi_2, Wheel(date: .now, moon: moon).progress)
+        XCTAssertEqual(.pi_2, Wheel(date: .now, moon: moon, correction: 0).progress)
         
         moon = .init(inclination: .pi_2, apparentAngle: 1)
         
         XCTAssertEqual(.lastQuarter, Moon.Phase(inclination: moon.inclination, angle: moon.apparentAngle))
-        XCTAssertEqual(.pi_2 + .pi, Wheel(date: .now, moon: moon).progress)
+        XCTAssertEqual(.pi_2 + .pi, Wheel(date: .now, moon: moon, correction: 0).progress)
+    }
+    
+    func testPoint() {
+        var moon = Moon(inclination: .pi, apparentAngle: 1)
+        XCTAssertEqual(1, Wheel(date: .now, moon: moon, correction: 0).point.x)
+        XCTAssertGreaterThan(0.1, Wheel(date: .now, moon: moon, correction: 0).point.y)
+        XCTAssertLessThan(-0.1, Wheel(date: .now, moon: moon, correction: 0).point.y)
         
-        moon = .init(inclination: .pi_2, apparentAngle: 1)
+        moon = .init(inclination: 0, apparentAngle: 0)
+        XCTAssertEqual(-1, Wheel(date: .now, moon: moon, correction: 0).point.x)
+        XCTAssertGreaterThan(0.1, Wheel(date: .now, moon: moon, correction: 0).point.y)
+        XCTAssertLessThan(-0.1, Wheel(date: .now, moon: moon, correction: 0).point.y)
         
-        XCTAssertEqual(.lastQuarter, Moon.Phase(inclination: moon.inclination, angle: moon.apparentAngle))
-        XCTAssertEqual(.pi_2 + .pi, Wheel(date: .now, moon: moon).progress)
+        moon = .init(inclination: .pi_2, apparentAngle: -1)
+        XCTAssertEqual(1, Wheel(date: .now, moon: moon, correction: 0).point.y)
+        XCTAssertGreaterThan(0.1, Wheel(date: .now, moon: moon, correction: 0).point.x)
+        XCTAssertLessThan(-0.1, Wheel(date: .now, moon: moon, correction: 0).point.x)
     }
     
     func testMove() {
         let date = Date()
         let moon = Moon(inclination: .pi, apparentAngle: 1)
-        let wheel = Wheel(date: date, moon: moon)
+        let wheel = Wheel(date: date, moon: moon, correction: 0)
         
         XCTAssertEqual(date,
                        wheel.move(radians: 0))
