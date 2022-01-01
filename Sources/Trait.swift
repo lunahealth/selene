a05e6ac063ccc2a07f23a00f77e1c2ee2574cbba
vaux <zero.griffin@gmail.com>
2022-01-01 13:12:08 +0100

    Create traits

---
+++ Sources/Trait.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public enum Trait {
+    case
+    period,
+    mood,
+    sleep,
+    nutrition,
+    workout,
+    walk,
+    focus,
+    water,
+    work,
+    eat,
+    sweets,
+    drink,
+    smoke
+}
