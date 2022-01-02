1fbc5fa07f800192b8c15d3ee1eaf8ce7da4657a
vaux <zero.griffin@gmail.com>
2022-01-02 13:33:02 +0100

    Add settings

---
+++ Sources/Settings.swift
@@ -0,0 +1,12 @@
+import Foundation
+import Archivable
+
+public struct Settings: Storable {
+    public var data: Data {
+        .init()
+    }
+    
+    public init(data: inout Data) {
+        
+    }
+}
