a3f51197fab044e8baa285fe1206989031b0d6a5
vaux <zero.griffin@gmail.com>
2022-03-10 12:32:37 +0100

    Remove equatable

--- Sources/Navigator.swift
+++ Sources/Navigator.swift
@@ -5,7 +5,7 @@ private let cycle = 29.53
 private let cycleRadians = .pi2 / cycle
 private let radius = 60.0
 
-public protocol Navigator: Equatable {
+public protocol Navigator {
     var origin: CGPoint { get }
     
     func approach(from point: CGPoint) -> CGPoint
