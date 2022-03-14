0df2c716752cba3422c2b1d871b285bf24a268ab
vaux <zero.griffin@gmail.com>
2022-03-14 11:44:32 +0100

    Update moon phases

--- Sources/Moon.Phase.swift
+++ Sources/Moon.Phase.swift
@@ -12,13 +12,13 @@ extension Moon {
         
         init(fraction: Int, angle: Double) {
             switch fraction {
-            case 0 ... 1:
+            case 0 ... 2:
                 self = .new
-            case 2 ... 48:
+            case 3 ... 47:
                 self = angle < 0 ? .waxingCrescent : .waningCrescent
-            case 49 ... 51:
+            case 48 ... 52:
                 self = angle < 0 ? .firstQuarter : .lastQuarter
-            case 52 ... 98:
+            case 53 ... 97:
                 self = angle < 0 ? .waxingGibbous : .waningGibbous
             default:
                 self = .full
