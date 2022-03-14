c1e97461d24519202c995cdf8e5df89cb975709a
vaux <zero.griffin@gmail.com>
2022-03-14 13:18:51 +0100

    Update tracker

--- Sources/Tracker.swift
+++ Sources/Tracker.swift
@@ -5,7 +5,7 @@ public struct Tracker: Navigator {
     public let origin: CGPoint
     
     public init(size: CGSize) {
-        origin = .init(x: size.width / 2, y: 65)
+        origin = .init(x: size.width / 2, y: 85)
     }
     
     public func approach(from point: CGPoint) -> CGPoint {
