0dec48983abb2951b47edde6fbfa8685d1c9c3d3
vaux <zero.griffin@gmail.com>
2022-03-14 14:47:37 +0100

    Add stats init

--- Sources/Stats.swift
+++ Sources/Stats.swift
@@ -1,4 +1,9 @@
 public struct Stats: Equatable {    
     public let level: Level
     public let percent: Double
+    
+    public init(level: Level, percent: Double) {
+        self.level = level
+        self.percent = percent
+    }
 }
